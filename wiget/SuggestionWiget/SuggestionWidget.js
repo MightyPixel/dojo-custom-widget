c48c444c6b5cf27c86b8327c65103f43019154be
Ognyan Angelov <ognyan_angelov@yahoo.com>
2013-08-07 19:11:27 +0300

    dojo config

--- wiget/SuggestionWiget/SuggestionWidget.js
+++ wiget/SuggestionWiget/SuggestionWidget.js
@@ -1,5 +1,5 @@
-define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./SuggestionWidget.html", "dojo/dom-style", "dojo/_base/fx", "dojo/request"],
-    function(on, declare, _WidgetBase, _TemplatedMixin, template, domStyle, baseFx, request){
+define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./SuggestionWidget.html", "dojo/dom-style", "dojo/_base/fx", "dojo/request", "dojo/_base/lang"],
+    function(on, declare, _WidgetBase, _TemplatedMixin, template, domStyle, baseFx, request, lang){
         return declare([_WidgetBase, _TemplatedMixin], {
             _inputNode: null,
             suggestionsNode: null,
@@ -18,7 +18,7 @@ define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMi
 
             _fillDropDown: function(words) {
                 console.log(words);
-            }
+            },
 
             _openDropDown: function() {
                 domStyle.set(domNode, "position", "relative");
@@ -27,15 +27,12 @@ define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMi
 
             _closeDropDown: function() {
                 domStyle.set(suggestionsNode, "display","none");
-            }
+            },
 
 
             postCreate: function(){
                 this.inherited(arguments);
                 console.log(this.inputNode);
-                request("words.json", {
-                    handleAs: "json"
-                }).then(_fillDropDown(words));
 
                 this.own(
                     on(this._inputNode, "keyup", lang.hitch(this, this._handleKeyUp))
