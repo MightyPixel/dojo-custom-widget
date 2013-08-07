f52e996841fbd3c19fb02e0b485d2fd529a568b0
LFirestorm <L.Firestorm@gmail.com>
2013-08-07 19:04:52 +0300

    stabilized configuration and paths

--- wiget/SuggestionWiget/SuggestionWidget.js
+++ wiget/SuggestionWiget/SuggestionWidget.js
@@ -1,5 +1,5 @@
-define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./SuggestionWidget.html", "dojo/dom-style", "dojo/_base/fx", "dojo/request"],
-    function(on, declare, _WidgetBase, _TemplatedMixin, template, domStyle, baseFx, request){
+define(["dojo/on", "dojo/_base/declare","dojo/_base/lang","dijit/_WidgetBase", "dijit/_TemplatedMixin", "dojo/text!./SuggestionWidget.html", "dojo/dom-style", "dojo/_base/fx", "dojo/request"],
+    function(on, declare, lang, _WidgetBase, _TemplatedMixin, template, domStyle, baseFx, request){
         return declare([_WidgetBase, _TemplatedMixin], {
             _inputNode: null,
             suggestionsNode: null,
@@ -18,7 +18,7 @@ define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMi
 
             _fillDropDown: function(words) {
                 console.log(words);
-            }
+            },
 
             _openDropDown: function() {
                 domStyle.set(domNode, "position", "relative");
@@ -27,15 +27,15 @@ define(["dojo/on", "dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMi
 
             _closeDropDown: function() {
                 domStyle.set(suggestionsNode, "display","none");
-            }
+            },
 
 
             postCreate: function(){
                 this.inherited(arguments);
                 console.log(this.inputNode);
-                request("words.json", {
+                request("data/words.json", {
                     handleAs: "json"
-                }).then(_fillDropDown(words));
+                }).then(this._fillDropDown);
 
                 this.own(
                     on(this._inputNode, "keyup", lang.hitch(this, this._handleKeyUp))
